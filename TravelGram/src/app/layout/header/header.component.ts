d86c299ea5087a424a54ce436209d096429f6d09
sasikumar6795 <32613154+sasikumar6795@users.noreply.github.com>
2021-03-17 12:39:34 +0530

    header and footer added

--- TravelGram/src/app/layout/header/header.component.ts
+++ TravelGram/src/app/layout/header/header.component.ts
@@ -1,15 +1,35 @@
 import { Component, OnInit } from '@angular/core';
-
+import { Router } from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
+import { AuthService } from 'src/app/services/auth.service';
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.css']
+  styleUrls: ['./header.component.css'],
 })
 export class HeaderComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
+  email: any;
+  constructor(
+    private authService: AuthService,
+    private toastrService: ToastrService,
+    private router: Router
+  ) {
+    authService.getUser().subscribe((user) => {
+      console.log('USER IS :', user);
+      this.email = user?.email;
+    });
   }
 
+  ngOnInit(): void {}
+
+  async handleSignOut() {
+    try {
+      await this.authService.signOut();
+      this.router.navigateByUrl('/signin');
+      this.toastrService.info('Logout Success');
+      this.email = null;
+    } catch (error) {
+      this.toastrService.error('Something went wrong');
+    }
+  }
 }
