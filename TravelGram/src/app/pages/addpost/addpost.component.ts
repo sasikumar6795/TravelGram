9f6d263155035d86684e131979b79bbe89162ffa
sasikumar6795 <32613154+sasikumar6795@users.noreply.github.com>
2021-03-24 19:24:57 +0530

    addpost started

--- TravelGram/src/app/pages/addpost/addpost.component.ts
+++ TravelGram/src/app/pages/addpost/addpost.component.ts
@@ -1,5 +1,29 @@
 import { Component, OnInit } from '@angular/core';
 
+//angular form
+import { NgForm } from '@angular/forms';
+
+//angular router
+import { Router } from '@angular/router';
+
+//services
+import { ToastrService } from 'ngx-toastr';
+import { AuthService } from 'src/app/services/auth.service';
+
+import { finalize } from 'rxjs/operators';
+
+//firebase
+import { AngularFireStorage } from '@angular/fire/storage';
+import { AngularFireDatabase } from '@angular/fire/database';
+
+//browser image resizer
+import { readAndCompressImage } from 'browser-image-resizer';
+import { imageConfig } from 'src/app/utils/config';
+
+//uuid
+
+import {v4 as uuidv4} from 'uuid';
+
 @Component({
   selector: 'app-addpost',
   templateUrl: './addpost.component.html',
@@ -7,7 +31,28 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AddpostComponent implements OnInit {
 
-  constructor() { }
+  location: string;
+  description: string;
+  picture: string =null;
+
+  user = null;
+  uploadPercent: number =null;
+
+  constructor(
+    private authService: AuthService,
+    private toastr: ToastrService,
+    private router : Router,
+    private db: AngularFireDatabase,
+    private storage: AngularFireStorage
+  ) { 
+
+    this.authService.getUser().subscribe((user) =>{
+      this.db.object(`/users/${user.uid}`)
+      .valueChanges()
+      .subscribe()
+    }
+    )
+  }
 
   ngOnInit(): void {
   }
