2dccef1a096ea3d674435d6724d66b517814a1d5
sasikumar6795 <32613154+sasikumar6795@users.noreply.github.com>
2021-03-24 17:39:53 +0530

    sign in component

--- TravelGram/src/app/pages/signin/signin.component.ts
+++ TravelGram/src/app/pages/signin/signin.component.ts
@@ -1,4 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { Router } from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
+import { AuthService } from 'src/app/services/auth.service';
+
+
 
 @Component({
   selector: 'app-signin',
@@ -7,9 +13,33 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SigninComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    private authService : AuthService,
+    private toastr : ToastrService,
+    private router : Router
+  ) { }
 
   ngOnInit(): void {
+  }
+
+  onSubmit(f: NgForm)
+  {
+    const {email, password} = f.form.value;
+
+    this.authService.signIn(email,password)
+    .then( (res) => {
+      console.log("sign in response"+ " " +res);
+      this.toastr.success('sign in success');
+      this.router.navigateByUrl('/'); 
+    }
+
+    )
+    .catch((err)=>{
+      console.log(err);
+      this.toastr.error(err.message, "",{
+        closeButton: true
+      } );
+    })
   }
 
 }
