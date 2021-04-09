37a4a9d74a76452293ca1483578696d9be9ce2da
sasikumar6795 <32613154+sasikumar6795@users.noreply.github.com>
2021-04-09 12:31:42 +0530

    implemented like and dislike features

--- TravelGram/src/app/components/post/post.component.ts
+++ TravelGram/src/app/components/post/post.component.ts
@@ -1,16 +1,83 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Input, OnInit , OnChanges, SimpleChanges } from '@angular/core';
+import { AngularFireDatabase } from '@angular/fire/database';
+
+import {
+  faThumbsUp,
+  faThumbsDown,
+  faShareSquare,
+} from '@fortawesome/free-regular-svg-icons';
+import { AuthService } from 'src/app/services/auth.service';
 
 @Component({
   selector: 'app-post',
   templateUrl: './post.component.html',
-  styleUrls: ['./post.component.css']
+  styleUrls: ['./post.component.css'],
 })
-export class PostComponent implements OnInit {
+export class PostComponent implements OnInit, OnChanges {
+  faThumbsUp = faThumbsUp;
+  faThumbsDown = faThumbsDown;
+  faShareSquare = faShareSquare;
 
-  @Input() post: [];
-  constructor() { }
-
-  ngOnInit(): void {
+  uid = null;
+  upVote = 0;
+  downVote = 0;
+  @Input() post;
+  constructor(
+    private authService: AuthService,
+    private db: AngularFireDatabase
+  ) {}
+  ngOnChanges(changes: SimpleChanges): void {
+    
+    if(this.post.vote)
+    {
+      Object.values(this.post.vote).map(
+        (val: any) => {
+          if(val.upVote)
+          {
+            this.upVote ++;
+          }
+          if(val.downVote)
+          {
+            this.downVote ++;
+          }
+        }
+      )
+    }
   }
 
+  ngOnInit(): void {
+
+    this.authService.getUser().subscribe(
+      (user)=> {
+        this.uid=user?.uid;
+      }
+    )
+
+  }
+
+  upVotePost()
+  {
+    console.log("upvoting");
+    this.db.object(`/posts/${this.post.id}/vote/${this.uid}`).set(
+      {
+        upVote:1,
+      }
+    )
+  }
+
+  downVotePost()
+  {
+    console.log("downvoting");
+    this.db.object(`/posts/${this.post.id}/vote/${this.uid}`).set(
+      {
+        downVote:1,
+      }
+    )
+  }
+
+
+  getInstaUrl()
+  {
+    return 'https://instagram.com/${this.post.instaId}'
+  }
 }
--- TravelGram/src/app/components/user/user.component.ts
+++ TravelGram/src/app/components/user/user.component.ts
@@ -7,7 +7,7 @@ import { Component, Input, OnInit } from '@angular/core';
 })
 export class UserComponent implements OnInit {
 
-  @Input() user = [];
+  @Input() user ;
   constructor() { }
 
   ngOnInit(): void {
